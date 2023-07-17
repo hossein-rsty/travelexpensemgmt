a6675633da9f651b01fa979984ffb91e43bab427
Hossein Rostamiraeini <rsty.hossein@gmail.com>
2023-07-17 18:17:27 +0200

    Initial commit

---
+++ eurekaserver/src/test/java/com/travelexpensemgmt/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelexpensemgmt.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ tripservice/src/test/java/com/travelexpensemgmt/tripservice/TripserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelexpensemgmt.tripservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
